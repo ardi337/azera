09bd9c8ca9a2fcdc78a7f4d047ce1161ca763e38
ardi337 <abdellahabdellah123456u@gmail.com>
2024-12-29 17:09:12 +0300

    Add files via upload

---
+++ usdr.py
@@ -0,0 +1,126 @@
+AUTOMATIC1111
+stable-diffusion-webui
+sd-webui
+sd-webui-controlnet
+stable-diffusion-webui-images-browser
+sd-webui
+sd-webui-cutoff
+sd-webui-regional-prompter
+sd-webui-tunnels
+sd-webui-roop
+pycloudflared
+insightface==0.7.3
+onnx==1.14.0
+onnxruntime==1.15.0
+opencv-python==4.7.0.72
+tqdm
+https://github.com/lllyasviel/stable-diffusion-webui-forge
+https://github.com/Iyashinouta/sd-model-downloader
+https://github.com/Coyote-A/ultimate-upscale-for-automatic1111
+https://github.com/Mikubill/sd-webui-controlnet
+https://github.com/A-I-Channel/public-link /home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui-forge/extensions/tunnels
+https://github.com/A-I-Channel/explorer-image
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui-forge/config.json
+
+/content
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/sonGokuDragonBallZSuper_v10.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/sonGokuDragonBallZSuper_v10.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/sonGokuDragonBallZSuper_v10.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/dbsuper_style_offset.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/dbsuper_style_offset.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/dbsuper_style_offset.jpg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/makotoShinkaiSubstyles_offset.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/makotoShinkaiSubstyles_offset.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/makotoShinkaiSubstyles_offset.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/studioGhibliStyle_offset.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/studioGhibliStyle_offset.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/studioGhibliStyle_offset.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/shingekiNoKyojinAttackOn_v1.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/shingekiNoKyojinAttackOn_v1.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/shingekiNoKyojinAttackOn_v1.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/The_Garden_of_Words.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/The_Garden_of_Words.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/The_Garden_of_Words.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/animeoutlineV4.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/animeoutlineV4.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/animeoutlineV4.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/cutelineart.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/cutelineart.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/cutelineart.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/linebaby.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/linebaby.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/linebaby.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/onePieceWanoSagaStyle_v2Offset.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/onePieceWanoSagaStyle_v2Offset.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/onePieceWanoSagaStyle_v2Offset.jpeg
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/add_detail.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/add_detail.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/add_detail.jpeg
+https://github.com/IDEA-Research/DWPose
+https://github.com/A-I-Channel/sdw-wd14-tagger
+https://github.com/fkunn1326/openpose-editor
+https://github.com/A-I-Channel/sd-openpose-editor-clone
+-b v2.5 https://github.com/camenduru/ui /home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui
+https://github.com/A-I-Channel/ui-clone /home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui
+https://github.com/A-I-Channel/AI_Channel_Theme
+https://github.com/deforum-art/sd-webui-deforum
+https://github.com/Bing-su/adetailer
+https://github.com/hnmr293/sd-webui-cutoff
+https://github.com/Gourieff/sd-webui-reactor
+https://huggingface.co/datasets/AI-C/Data-File/resolve/main/inswapper_128.onnx
+https://github.com/bit9labs/sd-ratio-lock
+https://github.com/brick2face/seamless-tile-inpainting
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui-forge/models/Stable-diffusion/cyberrealistic_v33.safetensors https://civitai.com/api/download/models/138176
+/home/studio-lab-user/sagemaker-studiolab-notebooks/stable-diffusion-webui-forge/extensions-builtin/sd-webui-controlnet/models/ioclab_sd15_recolor.safetensors https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/ioclab_sd15_recolor.safetensors
+install xformers==0.0.20 triton==2.0.0 -U
+uninstall -y torch
+uninstall -y torchvision
+uninstall -y torchaudio
+install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+apt -y update -qq
+apt -y install -qq aria2 libcairo2-dev pkg-config python3-dev
+apt-get update
+apt-get install -y google-perftools
+apt -y install wget git python3 python3-venv
+/home/studio-lab-user/sagemaker-studiolab-notebooks/
+pip install mediapipe
+pip install ultralytics
+pip install insightface==0.7.3
+--share --disable-safe-unpickle --theme=dark --no-half-vae --enable-insecure-extension --gradio-queue
+https://github.com/etherealxx/batchlinks-webui
+https://github.com/A-I-Channel/Downloader
+https://github.com/DominikDoom/a1111-sd-webui-tagcomplete
+-y -c conda-forge opencv
+https://github.com/pharmapsychotic/clip-interrogator-ext
+https://github.com/A-I-Channel/tagger-v2
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/21charturnerv2.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/Asian-Less-Neg.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/bad-artist-anime.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/bad-artist.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/bad-hands-5.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/bad-image-v2-39000.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/bad-picture-chill-75v.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/BadDream.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/badhandv4.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/bad_pictures.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/bad_prompt.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/bad_prompt_version2.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/charturnerv2.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/CyberRealistic_Negative-neg.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/easynegative.safetensors
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/EasyNegativeV2.safetensors
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/epiCNegative.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/epiCRealism.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/FastNegativeEmbedding.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/HyperStylizeV6.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/nartfixer.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/negative_hand-neg.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/nfixer.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/ng_deepnegative_v1_75t.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/nrealfixer.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/pureerosface_v1.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/rmadanegative402_sd15-neg.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/ulzzang-6500-v1.1.bin
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/ulzzang-6500.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/UnrealisticDream.pt
+https://raw.githubusercontent.com/A-I-Channel/Data-file/main/embeddings/verybadimagenegative_v1.3.pt
